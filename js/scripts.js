938fe71f010fb8667a357b81375c639bc541f150
virsail <virsaileric@gmail.com>
2020-08-01 16:30:30 +0300

    add function for enabling user option to display in the text box

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ function ddselect()
     var displayText=d.options[d.selectedIndex].text;
     document.getElementById("textvalue").value=displayText;
 }
+
+
+function ddselect1()
+{
+    var d=document.getElementById("ddselect1");
+    var displayText=d.options[d.selectedIndex].text;
+    document.getElementById("textvalue1").value=displayText;
+}
