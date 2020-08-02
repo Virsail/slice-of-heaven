0e6c2194c934f40d841b7724cd2c40ef1edfa227
virsail <virsaileric@gmail.com>
2020-08-02 10:36:23 +0300

    add an object for pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//function for first id ddselect//
 function ddselect()
 {
     var d=document.getElementById("ddselect");
@@ -5,10 +6,24 @@ function ddselect()
     document.getElementById("textvalue").value=displayText;
 }
 
-
+//function for second id ddselect1//
 function ddselect1()
 {
     var d=document.getElementById("ddselect1");
     var displayText=d.options[d.selectedIndex].text;
     document.getElementById("textvalue1").value=displayText;
 }
+
+//object for pizza//
+var pizza = {
+    size: ["Large", "Small", "Medium"],
+    crust: ["Crispy", "Stuffed", "Gluten free", "Scrumbled"],
+    toppings: ["pepperoni","mushrooms","onions", "sausage","bacon", "extra cheese"],
+  };  
+
+  //object for pizza prices,crust and toppings//
+  var pizzaPrices = {
+    size: ["Large = 800", "Small = 200", "Medium = 400"],
+    crust: ["Crispy  = 400", "Stuffed = 350", "Gluten free = 300", "Scrumbled = 250"],
+    toppings: ["pepperoni = 600","mushrooms = 550","onions = 500", "sausage = 450","bacon = 400", "extra cheese = 350"],
+  }
